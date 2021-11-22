15f8003cf6e2a65e28450d95703c77d59deabebe
Артём Оконечников <temaweb@yandex.ru>
2021-11-23 01:24:58 +0300

    Initial commit cmake project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Nintendo Entertainment System
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+![C/C++ CI](https://github.com/temaweb/NES-6502/workflows/C/C++%20CI/badge.svg)
---
+++ src/main.cc
@@ -0,0 +1,21 @@
+/*
+ * This file is part of the NES-6502 distribution (https://github.com/temaweb/NES-6502).
+ * Copyright (c) 2021 Artem Okonechnikov.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+int main()
+{
+    
+}
